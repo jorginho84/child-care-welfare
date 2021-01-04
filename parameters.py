2519ae787089188e1fdb4fbda45c8849409ea22a
antoaguilerac <antoaguilerac@gmail.com>
2021-01-04 14:01:24 -0300

    31-12-20
    - utility: crea funciones para simular salario, shocks, score, ingreso no laboral y utilidad.
    - simdata: simula elección de labor choice y cc choice a partir de maximización de utilidad.
    - estimate: llama a simdata para estimar momentos relevantes.
    - bootstrap: calcula momentos relevantes de la data mediante bootstrap.
    - parameters: alberga parámetros necesarios para utilizar en las simulaciones.

--- parameters.py
+++ parameters.py
@@ -9,13 +9,16 @@ class Parameters:
 	List of structural parameters and prices
 
 	"""
-    def __init__(self, betas, sigmaw, meanshocks, covshocks, T, Lc, alpha, gamma):
+    def __init__(self, betasw, betast, betasn, sigma2w, sigma2t, sigma2n, meanshocks, covshocks, T, Lc, alpha, gamma):
+         
+        self.betasw, self.betast, self.betasn = betasw, betast, betasn
         
-        self.betas, self.sigmaw,        = betas, sigmaw
+        self.sigma2w, self.sigma2t, self.sigma2n = sigma2w, sigma2t, sigma2n
         
         self.T, self.Lc                 = T, Lc
         
         self.meanshocks, self.covshocks = meanshocks, covshocks
         
         self.alpha, self.gamma          = alpha, gamma
-    
+        
+ 
