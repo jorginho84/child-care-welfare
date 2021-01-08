73716f21fb983d392dd436cfd2fcf4766e8afb08
antoaguilerac <antoaguilerac@gmail.com>
2021-01-08 15:10:30 -0300

    Revert "31-12-20"
    This reverts commit 2519ae787089188e1fdb4fbda45c8849409ea22a.

--- parameters.py
+++ parameters.py
@@ -9,16 +9,13 @@ class Parameters:
 	List of structural parameters and prices
 
 	"""
-    def __init__(self, betasw, betast, betasn, sigma2w, sigma2t, sigma2n, meanshocks, covshocks, T, Lc, alpha, gamma):
-         
-        self.betasw, self.betast, self.betasn = betasw, betast, betasn
+    def __init__(self, betas, sigmaw, meanshocks, covshocks, T, Lc, alpha, gamma):
         
-        self.sigma2w, self.sigma2t, self.sigma2n = sigma2w, sigma2t, sigma2n
+        self.betas, self.sigmaw,        = betas, sigmaw
         
         self.T, self.Lc                 = T, Lc
         
         self.meanshocks, self.covshocks = meanshocks, covshocks
         
         self.alpha, self.gamma          = alpha, gamma
-        
- 
+    
--- utility.py
+++ utility.py
@@ -42,14 +42,7 @@ class Utility(object):
         Returns wage as a column vector
         """
 
-<<<<<<< HEAD
-        epsilon = np.sqrt(self.param.sigma2w)*np.random.randn(self.N)
-                
-        xw = np.concatenate((np.reshape(np.array(self.data['constant'], dtype=np.float64),(self.N,1)),
-                             np.reshape(np.array(self.data['m_sch'], dtype=np.float64), (self.N,1)),), axis=1)
-=======
         epsilon = np.sqrt(self.param.sigmaw)*np.random.randn(self.N)
->>>>>>> parent of 56b0066... Semana 15-Dic
         
         #constant is always the last column:
         colbeta0 = len(self.data[0])-1
@@ -57,7 +50,7 @@ class Utility(object):
         xw = np.concatenate((np.reshape(self.data[:,0], (self.N,1)), #escolaridad
                             np.reshape(self.data[:,colbeta0], (self.N,1)),),axis=1) #constante
         
-        betas = self.param.betasw
+        betas = self.param.betas
         
 
         return np.reshape(np.exp(np.dot(xw,betas)+ epsilon),(self.N,1))
@@ -68,7 +61,7 @@ class Utility(object):
      
         Computes \theta y \omega from a bivariate normal distribution using a given mean and covariance matrix
         
-        Returns array with objects omega [0] and theta [1]
+        Returns array with objects omega (0) and theta (1)
         """
         
         s = np.random.multivariate_normal(self.param.meanshocks, self.param.covshocks, self.N)
@@ -78,48 +71,7 @@ class Utility(object):
         return omega, theta
 
 
-<<<<<<< HEAD
-    def score(self, cc_choice, shocks):
-        """
-        Computes cognitive test score 
-        
-        Sc = beta_0+theta*d_cc_34+u
-        """
-        u = np.reshape(np.random.randn(self.N), (self.N,1))
-        ones = np.reshape(np.ones(self.N), (self.N,1))
-
-        return  self.param.betast[0]*ones + np.multiply(shocks[1], cc_choice) + u
-     
-    
-    
-    def nli(self, labor_choice):
-        """
-        Computes non-labor income 
-        
-        d_married : dummy that equals 1 when mother is married; 0 otherwise
-        d_work    : dummy that equals 1 when mother chooses to work; 0 otherwise
-        n_kids    : number of children
-        sch       : years of schooling
-        
-        ln_nli = beta_0+beta_1*d_married+beta_2*d_work+beta_3*n_kids+beta_4*sch+w
-        """
-        
-        epsilon = np.random.randn(self.N)
-        xn = np.concatenate((np.reshape(np.array(self.data['constant'], dtype=np.float64),(self.N,1)),
-                             np.reshape(np.array(self.data['married34'], dtype=np.float64), (self.N,1)),
-                             np.reshape(np.array(labor_choice, dtype=np.float64), (self.N,1)),
-                             np.reshape(np.array(self.data['tot_kids'], dtype=np.float64), (self.N,1)),
-                             np.reshape(np.array(self.data['m_sch'], dtype=np.float64), (self.N,1)),), axis=1)
-      
-        betas = self.param.betasn
-        
-        return np.reshape(np.exp(np.dot(xn,betas)+ epsilon),(self.N,1))
-
-    
-    def utility(self, wage, shocks, H, D):
-=======
     def utility(self, shocks, wage, H, D):
->>>>>>> parent of 56b0066... Semana 15-Dic
         """
         
         L_i     : "leisure" understood as time not working, sleeping, commuting or spent in cc
@@ -136,16 +88,10 @@ class Utility(object):
         Computes nu: 
         nu = -omega + gamma*theta
         
-        Income: 
-        income = labor income + non labor income = wage*hours + nli
-        
         Returns column vector of Utility, computed as:
             U = ln(wage*H) + alpha*L + D*nu
 
         """
-        d_work = H/160
-        nli = self.nli(d_work)
-        income = wage*H + nli
         
         C = D*np.reshape(self.data[:,1], (self.N, 1)) #commute*dummy cc
         
@@ -156,13 +102,9 @@ class Utility(object):
         
         L = self.param.T - (1-D)*self.param.Lc - H - 2*C
         
-<<<<<<< HEAD
-        return np.log(income) + L*alpha + D*nu
-=======
         return np.log(wage*H + sys.float_info.epsilon) + L*alpha + D*nu
 
 
->>>>>>> parent of 56b0066... Semana 15-Dic
 
 
 
